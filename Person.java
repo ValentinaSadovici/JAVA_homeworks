2b7bf74e594880d0720a2f158d20f5bd3788a673
ValentinaSadovici <126664252+ValentinaSadovici@users.noreply.github.com>
2023-03-25 11:48:43 +0100

    Java Class

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    public static void main(String[] args) {
+
+        String name;
+        String surname;
+        int yearOfBirth;
+
+        name = "Alice";
+        surname = "Smith";
+        yearOfBirth = 1999;
+
+        System.out.println(name + " " + surname + " was born in " + (yearOfBirth) + ".");
+    }
+}
